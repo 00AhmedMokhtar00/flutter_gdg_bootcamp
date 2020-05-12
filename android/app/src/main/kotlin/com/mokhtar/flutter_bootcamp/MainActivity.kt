c075fc4560a1b1079f23ff3d7cba615d5c001980
Ahmed Mokhtar <ahmedmokhtar7711@gmail.com>
2020-05-13 00:27:53 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/mokhtar/flutter_bootcamp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mokhtar.flutter_bootcamp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/mokhtar/flutterbootcamp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mokhtar.flutterbootcamp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
